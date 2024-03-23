32cce8a0ca0ba22e58ddafd7f1b00f4fc9f625b2
Moyses Azevedo <m.mirazev@gmail.com>
2024-03-22 21:48:16 -0300

    ♻️ refactor: finished

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -1,11 +1,15 @@
 from Application.create_financial_json import CreateFinancialJson
 from Utils.create_json import CreateJson
+import json
 
-create_financial_json = CreateFinancialJson()
-create_financial_json.generateFinancialJSON()
-financialJson = create_financial_json.get_financialJson()
-CreateJson(financialJson, "Infra/Jsons/Financial.json")
+#create_financial_json = CreateFinancialJson()
+#create_financial_json.generateFinancialJSON()
+#financialJson = create_financial_json.get_financialJson()
+#CreateJson(financialJson, "Infra/JSONs/Financial.json")
+#print(financialJson)
 
 
+with open('Infra/JSONs/Financial.json', 'r') as file:
+    financialJson = json.load(file)
 
 print(financialJson)
